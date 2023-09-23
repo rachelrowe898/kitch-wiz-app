2db17521d3719a2b0bb8b70edb88fd19f4dbe175
Amanda Liu <amandastephanieliu@gmail.com>
2023-09-23 16:37:45 -0400

    1.2 photorealistic mockup

--- app/script.js
+++ app/script.js
@@ -1,3 +1,15 @@
+function openNav() {
+  var x = document.getElementById("menuLinks");
+  var recipeSection = document.getElementById("recipe-section");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    recipeSection.style.marginTop = "150px";
+  } else {
+    x.style.display = "block";
+    recipeSection.style.marginTop = "300px";
+  }
+}
+
 function switchTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
