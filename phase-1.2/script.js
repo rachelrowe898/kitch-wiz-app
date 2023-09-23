7a5e06fd8052b128db8b3697b6c32028233c4aa5
Amanda Liu <amandastephanieliu@gmail.com>
2023-09-23 09:47:23 -0400

    update style

--- phase-1.2/script.js
+++ phase-1.2/script.js
@@ -1,8 +1,7 @@
 function switchTab(evt, tabName) {
-  // Declare all variables
   var i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
+  // Hide all elements with class="tabcontent"
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
