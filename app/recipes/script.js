e969a462afe07100f3d5794b87c5cc220df19b27
Amanda Liu <amandastephanieliu@gmail.com>
2023-10-11 08:05:39 -0400

    add filter time slider

--- app/recipes/script.js
+++ app/recipes/script.js
@@ -10,21 +10,38 @@ function openNav() {
   }
 }
 
-function openAdd() {
-  console.log("open add");
+function closeAdd() {
   var x = document.getElementById("addRecipe");
   var addIcon = document.getElementById("add-icon");
-  var sortDropdown = document.getElementsByClassName("dropdown-menu")[0];
-  var sortDropdownButton = document.getElementsByClassName("sortDropdownButton")[0];
+  x.style.display = "none";
+  addIcon.style.color = "#208AAE";
+}
+
+function closeFilter() {
+  var x = document.getElementById("filterMenu");
+  var filterIcon = document.getElementById("filter-icon");
+  x.style.display = "none";
+  filterIcon.style.color = "#208AAE";
+}
+
+function closeSort() {
+  var x = document.getElementById("sortMenu");
+  var sortButton = document.getElementsByClassName("sortDropdownButton")[0];
+  x.style.display = "none";
+  sortButton.style.backgroundColor = "#208AAE";
+}
+
+function openAdd() {
+  var x = document.getElementById("addRecipe");
+  var addIcon = document.getElementById("add-icon");
   if (x.style.display === "block") {
     x.style.display = "none";
     addIcon.style.color = "#208AAE";
   } else {
     x.style.display = "block";
     addIcon.style.color = "#fdb833";
-    sortDropdown.style.display = "none";
-    sortDropdownButton.style.backgroundColor = "#208AAE";
-    // recipeListing.style.display = "none";
+    closeFilter();
+    closeSort();
   }
 }
 
@@ -91,15 +108,15 @@ function saveRecipe() {
   }
 }
 
-function flipSort() {
-  console.log("flip sort");
-  var x = document.getElementById("sort-icon");
-  if (x.className === "fa fa-sort-amount-asc") {
-    x.className = "fa fa-sort-amount-desc";
-  } else {
-    x.className = "fa fa-sort-amount-asc"; 
-  }
-}
+// function flipSort() {
+//   console.log("flip sort");
+//   var x = document.getElementById("sort-icon");
+//   if (x.className === "fa fa-sort-amount-asc") {
+//     x.className = "fa fa-sort-amount-desc";
+//   } else {
+//     x.className = "fa fa-sort-amount-asc"; 
+//   }
+// }
 
 function switchTab(evt, tabName) {
   // Declare all variables
@@ -125,22 +142,22 @@ function switchTab(evt, tabName) {
   console.log(evt.currentTarget.className)
 }
 
-function openDropdown() {
+function openSort() {
   // Get the dropdown button element
   const dropdownButton = document.getElementsByClassName('sortDropdownButton')[0];
 
-  var dropdownMenu = document.getElementsByClassName("dropdown-menu")[0];
+  var dropdownMenu = document.getElementById("sortMenu");
   var addRecipe = document.getElementById("addRecipe");
   var addIcon = document.getElementById("add-icon");
 
   if (dropdownMenu.style.display === "none") {
-    var dropdownMenu = document.getElementsByClassName("dropdown-menu")[0];
+    var dropdownMenu = document.getElementById("sortMenu");
     dropdownMenu.style.display = "block";
     dropdownButton.style.backgroundColor = "#fdb833";
-    addRecipe.style.display = "none";
-    addIcon.style.color = "#208AAE";
+    closeAdd();
+    closeFilter();
   } else {
-    var dropdownMenu = document.getElementsByClassName("dropdown-menu")[0];
+    var dropdownMenu = document.getElementById("sortMenu");
     dropdownMenu.style.display = "none";
     dropdownButton.style.backgroundColor = "#208AAE";
   }
@@ -179,9 +196,16 @@ function sortRecipes(order) {
     console.log("timeA", timeA);
     console.log("timeB", timeB);
 
+    sortMenuAsc = document.getElementById("sortMenuAsc");
+    sortMenuDesc = document.getElementById("sortMenuDesc");
+
     if (order === 'asc') {
+      sortMenuAsc.style.color = "#fdb833";
+      sortMenuDesc.style.color = "white";
       return timeA - timeB;
     } else if (order === 'desc') {
+      sortMenuDesc.style.color = "#fdb833";
+      sortMenuAsc.style.color = "white";
       return timeB - timeA;
     }
   });
@@ -198,3 +222,107 @@ function sortRecipes(order) {
     container.appendChild(square);
   });
 }
+
+function openFilter() {
+  var x = document.getElementById("filterMenu");
+  var filterIcon = document.getElementById("filter-icon");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    filterIcon.style.color = "#208AAE";
+  } else {
+    x.style.display = "block";
+    filterIcon.style.color = "#fdb833";
+    closeAdd();
+    closeSort();
+    // recipeListing.style.display = "none";
+  }
+}
+
+function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, '#C6C6C6', '#FDB833', controlSlider);
+  if (from > to) {
+      fromSlider.value = to;
+      fromInput.value = to;
+  } else {
+      fromSlider.value = from;
+  }
+}
+  
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+  const [from, to] = getParsed(fromInput, toInput);
+  fillSlider(fromInput, toInput, '#C6C6C6', '#FDB833', controlSlider);
+  setToggleAccessible(toInput);
+  if (from <= to) {
+      toSlider.value = to;
+      toInput.value = to;
+  } else {
+      toInput.value = from;
+  }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+const [from, to] = getParsed(fromSlider, toSlider);
+fillSlider(fromSlider, toSlider, '#C6C6C6', '#FDB833', toSlider);
+if (from > to) {
+  fromSlider.value = to;
+  fromInput.value = to;
+} else {
+  fromInput.value = from;
+}
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+const [from, to] = getParsed(fromSlider, toSlider);
+fillSlider(fromSlider, toSlider, '#C6C6C6', '#FDB833', toSlider);
+setToggleAccessible(toSlider);
+if (from <= to) {
+  toSlider.value = to;
+  toInput.value = to;
+} else {
+  toInput.value = from;
+  toSlider.value = from;
+}
+}
+
+function getParsed(currentFrom, currentTo) {
+const from = parseInt(currentFrom.value, 10);
+const to = parseInt(currentTo.value, 10);
+return [from, to];
+}
+
+/* filter slider code */
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+  const rangeDistance = to.max-to.min;
+  const fromPosition = from.value - to.min;
+  const toPosition = to.value - to.min;
+  controlSlider.style.background = `linear-gradient(
+    to right,
+    ${sliderColor} 0%,
+    ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
+    ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
+    ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
+    ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+    ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+const toSlider = document.querySelector('#toSlider');
+if (Number(currentTarget.value) <= 0 ) {
+  toSlider.style.zIndex = 2;
+} else {
+  toSlider.style.zIndex = 0;
+}
+}
+
+const fromSlider = document.querySelector('#fromSlider');
+const toSlider = document.querySelector('#toSlider');
+const fromInput = document.querySelector('#fromInput');
+const toInput = document.querySelector('#toInput');
+fillSlider(fromSlider, toSlider, '#C6C6C6', '#FDB833', toSlider);
+setToggleAccessible(toSlider);
+
+fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
