1fbe3c1e6799a0250cc3cf45a99e5cba9936d186
Robert <rchen129@umd.edu>
2023-10-12 14:10:16 -0400

    Patch User Profiles to look better

--- app/User Profile/script.js
+++ app/User Profile/script.js
@@ -1,17 +1,11 @@
-// Get the form element
-const profileForm = document.getElementById('profile-form');
-
-// Add an event listener for form submission
-profileForm.addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent the form from submitting normally
-
-    // Get the values from the form
-    const preferences = document.getElementById('preferences').value;
-    const allergies = document.getElementById('allergies').value;
-    const difficulty = document.getElementById('difficulty').value;
-
-    // Perform any necessary actions here, such as sending the data to a server or updating a database
-
-    // Optionally, display a success message to the user
-    alert('Profile updated successfully!');
-});
+function openNav() {
+    var x = document.getElementById("menuLinks");
+    var userSection = document.getElementById("user-profile-section");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+      userSection.style.marginTop = "150px";
+    } else {
+      x.style.display = "block";
+      userSection.style.marginTop = "300px";
+    }
+  }
