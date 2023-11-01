b390e4e5757e8507ef408759007975be5b6feaf0
Bobby Chen <bobbysampson47@gmail.com>
2023-11-01 15:50:41 -0400

    Update Profile Save Popup

--- app/User Profile/script.js
+++ app/User Profile/script.js
@@ -13,9 +13,23 @@ function openNav() {
 function saveUserProfile() {
   // Get the selected values from the form fields
   var foodPreference = document.getElementById("food-preference").value;
-  var allergies = document.getElementById("allergies").value;
+  var allergies = []; // Create an empty array to store the selected allergy values
   var difficulty = document.getElementById("difficulty").value;
   var diet = document.getElementById("diet").value;
+
+  // Get the checked allergy checkboxes
+  var allergyCheckboxes = document.querySelectorAll("input[type='checkbox'][name='allergies']:checked");
+
+  // Loop through the checked allergy checkboxes and add their values to the allergies array
+  for (var i = 0; i < allergyCheckboxes.length; i++) {
+    allergies.push(allergyCheckboxes[i].value);
+  }
+
+  // Get the value of the other allergy text box, if it is checked
+  var otherAllergyCheckbox = document.getElementById("otherAllergyCheckbox");
+  if (otherAllergyCheckbox.checked) {
+    allergies.push(document.getElementById("otherAllergy").value);
+  }
 
   // log the values to the console.
   console.log("Food Preference: " + foodPreference);
@@ -23,8 +37,9 @@ function saveUserProfile() {
   console.log("Preparation Difficulty: " + difficulty);
   console.log("Diet: " + diet);
 
-  showPopup("User profile is saved!"); // Show the popup when the user profile is saved
+  showPopup("User profile is saved! Your profile is Food Preference: " + foodPreference + ", Allergies: " + allergies + ", Preparation Difficulty: " + difficulty + ", Diet: " + diet + "."); 
 }
+
 
 function showPopup(message) {
   var messageDisplay = document.createElement("div");
@@ -55,7 +70,14 @@ function openAddProfile() {
     newGroupTemplate.classList.remove('hidden');
 }
 
-function openDeleteProfile() {
+function leaveUserGroupNH() {
+  const leaveGroupButton = document.getElementById('leave-group-button');
+  const groupElement = document.getElementById('nerdHerd');
+
+  leaveGroupButton.addEventListener('click', function(){
+    groupElement.style.display = 'none';
+  });
+  window.location.href = "userGroups.html";
 
 }
 
@@ -65,8 +87,9 @@ function createUserGroup() {
 
   const groupName = groupNameInput.value;
   const groupMembers = groupMembersInput.value;
+  console.log(groupName, groupMembers)
   // Update the group name in the revealed template
-  const newGroupName = document.getElementById('new-group-name');
+  var newGroupName = document.getElementById('new-group-name');
   newGroupName.textContent = groupName;
 
   // Redirect back to userGroups.html
